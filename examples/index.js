9865367d102c726f099c0cfa2155289d1110ffb2
Nate Higgins <nat@nath.is>
2019-07-04 18:31:54 +0100

    version 0.0.2: rewrite implementation using a single container component without depending on useEffect. Should now be concurrent safe

--- examples/index.js
+++ examples/index.js
@@ -3,28 +3,28 @@ import ReactDOM from "react-dom";
 import produce from "immer";
 import uuid from "uuid/v4";
 
-import { useLocus, LocusProvider } from "../src/react-locus";
+import Locus, { useLocus } from "../src/react-locus";
 
-function KeylineItem({ id, add, remove }) {
-  const { ready, position, last } = useLocus();
+function KeylineItem({ id, add, remove, up, down }) {
+  const { position, last } = useLocus();
 
   return (
-    ready && (
-      <div
-        style={{
-          border: "1px solid",
-          borderTopWidth: 1,
-          borderBottomWidth: last ? 1 : 0,
-          display: "flex",
-          padding: 20
-        }}
-      >
-        #{position} ({id.split("-")[0]})
-        <div style={{ flex: 1 }} />
-        <button onClick={() => add(id)}>+</button>
-        <button onClick={() => remove(id)}>-</button>
-      </div>
-    )
+    <div
+      style={{
+        border: "1px solid",
+        borderTopWidth: 1,
+        borderBottomWidth: last ? 1 : 0,
+        display: "flex",
+        padding: 20
+      }}
+    >
+      #{position} ({id.split("-")[0]})
+      <div style={{ flex: 1 }} />
+      <button onClick={() => add(id)}>+</button>
+      <button onClick={() => remove(id)}>-</button>
+      <button onClick={() => up(id)}>▲</button>
+      <button onClick={() => down(id)}>▼</button>
+    </div>
   );
 }
 
@@ -43,12 +43,33 @@ function App() {
 
   const remove = id => setItems(items => items.filter(item => item !== id));
 
+  const up = id =>
+    setItems(
+      produce(items => {
+        const index = items.indexOf(id);
+
+        if (index !== 0) {
+          items.splice(index - 1, 2, id, items[index - 1]);
+        }
+      })
+    );
+  const down = id =>
+    setItems(
+      produce(items => {
+        const index = items.indexOf(id);
+
+        if (index < items.length - 1) {
+          items.splice(index, 2, items[index + 1], id);
+        }
+      })
+    );
+
   return (
-    <LocusProvider>
-      {items.map((id, idx) => (
-        <KeylineItem key={idx} id={id} remove={remove} add={add} />
+    <Locus>
+      {items.map(id => (
+        <KeylineItem key={id} {...{ id, up, down, remove, add }} />
       ))}
-    </LocusProvider>
+    </Locus>
   );
 }
 
--- src/react-locus.js
+++ src/react-locus.js
@@ -1,2 +1,44 @@
-export { default as useLocus } from "./use-locus";
-export { default as LocusProvider } from "./locus-provider";
+import React from "react";
+
+const LocusContext = React.createContext(null);
+
+export function useLocus() {
+  const locus = React.useContext(LocusContext);
+
+  if (!locus) {
+    throw new Error(`Cannot use locus outside of a <Locus> component`);
+  }
+
+  return locus;
+}
+
+function LocusItem({ children, locusData }) {
+  const { position, total } = locusData;
+  const first = position === 0;
+  const last = position === total - 1;
+  const only = first && last;
+  const locus = { position, total, first, last, only };
+
+  return (
+    <LocusContext.Provider value={locus}>{children}</LocusContext.Provider>
+  );
+}
+
+function Locus({ children }) {
+  const childArr = React.Children.toArray(children);
+  const mappedChildren = childArr.map((child, idx) => (
+    <LocusItem
+      key={idx}
+      locusData={{
+        position: idx,
+        total: childArr.length
+      }}
+    >
+      {child}
+    </LocusItem>
+  ));
+
+  return <>{mappedChildren}</>;
+}
+
+export default Locus;
